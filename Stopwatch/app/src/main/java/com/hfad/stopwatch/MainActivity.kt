38698f5ee65f61883f17cc53b7fddc1e65ddfbb2
Nezari <Nezari@Nezari>
2023-11-18 01:21:41 +0700

    add main vars

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hfad.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById(R.id.stopwatch)
     }
 }
