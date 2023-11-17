726a562bc21cb08f7673aede24a602b07735be83
Nezari <Nezari@Nezari>
2023-11-18 01:42:32 +0700

    add vals and if for bundle

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -11,11 +11,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var stopwatch: Chronometer
     var running = false
     var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById(R.id.stopwatch)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.stop()
+            } else setBaseTime()
+        }
 
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener {
