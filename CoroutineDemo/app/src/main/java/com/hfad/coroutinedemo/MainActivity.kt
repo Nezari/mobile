c41eec411dab6523430a955abbbbc1b5765697d5
Nezari <Nezari@Nezari>
2023-11-18 03:39:05 +0700

    add suspend fun performTask

--- CoroutineDemo/app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
+++ CoroutineDemo/app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
@@ -5,11 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import com.hfad.coroutinedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var count: Int = 1
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    suspend fun performTask(tasknumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5_000)
+            return@async "Finished Coroutine $tasknumber"
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
