06b475ae0bdc323256a44bd6365fb87cfe3fa86e
Nezari <Nezari@Nezari>
2023-12-25 00:55:48 +0700

    class CreateCard minor change

--- ToDoList/app/src/main/java/com/example/todolist/CreateCard.kt
+++ ToDoList/app/src/main/java/com/example/todolist/CreateCard.kt
@@ -7,9 +7,11 @@ import androidx.room.Room
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.android.synthetic.main.activity_create_card.*
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class CreateCard : AppCompatActivity() {
     private lateinit var database: Database
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_card)
@@ -20,8 +22,8 @@ class CreateCard : AppCompatActivity() {
             if (create_title.text.toString().trim { it <= ' ' }.isNotEmpty()
                 && create_priority.text.toString().trim { it <= ' ' }.isNotEmpty()
             ) {
-                var title = create_title.getText().toString()
-                var priority = create_priority.getText().toString()
+                val title = create_title.text.toString()
+                val priority = create_priority.text.toString()
                 DataObject.setData(title, priority)
                 GlobalScope.launch {
                     database.dao().insertTask(Entity(0, title, priority))
