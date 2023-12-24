bbe57b29ec5392e780fba429a01a8167e6276730
Nezari <Nezari@Nezari>
2023-12-25 00:59:14 +0700

    splash screen minor changes

--- ToDoList/app/src/main/java/com/example/todolist/SplashScreen.kt
+++ ToDoList/app/src/main/java/com/example/todolist/SplashScreen.kt
@@ -1,16 +1,20 @@
 package com.example.todolist
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     private lateinit var database: Database
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -25,5 +29,4 @@ class SplashScreen : AppCompatActivity() {
             startActivity(intent)
         }, 2000)
     }
-
 }
