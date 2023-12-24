64c1525bb1fa3cca28d2630fd431da66c2b42dab
Nezari <Nezari@Nezari>
2023-12-24 23:57:16 +0700

    create dao and queries

---
+++ ToDoList/app/src/main/java/com/example/todolist/DAO.kt
@@ -0,0 +1,26 @@
+package com.example.todolist
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+class DAO {
+    @Insert
+    suspend fun insertTask(entity: Entity)
+
+    @Update
+    suspend fun updateTask(entity: Entity)
+
+    @Delete
+    suspend fun deleteTask(entity: Entity)
+
+    @Query("DELETE FROM to_do_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM to_do_table")
+    suspend fun getTasks():List<CardInfo>
+}
