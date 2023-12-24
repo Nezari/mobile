577351d9f5ad5779765841cedaca9a3c2b495a4a
Nezari <Nezari@Nezari>
2023-12-25 04:14:44 +0700

    bug fix

--- ToDoList/app/src/main/java/com/example/todolist/Adapter.kt
+++ ToDoList/app/src/main/java/com/example/todolist/Adapter.kt
@@ -21,19 +21,18 @@ class Adapter(private var data: List<CardInfo>) : RecyclerView.Adapter<Adapter.V
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         when (data[position].priority.lowercase()) {
-            "high" -> holder.layout?.setBackgroundColor(Color.parseColor("#F05454"))
-            "medium" -> holder.layout?.setBackgroundColor(Color.parseColor("#EDC988"))
+            "высокий" -> holder.layout?.setBackgroundColor(Color.parseColor("#F05454"))
+            "средний" -> holder.layout?.setBackgroundColor(Color.parseColor("#EDC988"))
             else -> holder.layout?.setBackgroundColor(Color.parseColor("#00917C"))
         }
 
-        holder.title?.text ?: data[position].title
-        holder.priority?.text ?: data[position].priority
+        holder.title?.text = data[position].title
+        holder.priority?.text = data[position].priority
         holder.itemView.setOnClickListener{
-            val intent= Intent(holder.itemView.context,UpdateCard::class.java)
-            intent.putExtra("id",position)
+            val intent = Intent(holder.itemView.context, UpdateCard::class.java)
+            intent.putExtra("id", position)
             holder.itemView.context.startActivity(intent)
         }
-
     }
 
     override fun getItemCount(): Int {
