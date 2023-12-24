2625b5923e1451b811f8521bb3e270f701ba013a
Nezari <Nezari@Nezari>
2023-12-25 00:38:17 +0700

    add main code

--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: Database
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        database = Room.databaseBuilder(
+            applicationContext, Database::class.java, "to_do_db"
+        ).build()
+        add.setOnClickListener {
+            val intent = Intent(this, CreateCard::class.java)
+            startActivity(intent)
+        }
+        deleteAll.setOnClickListener {
+            DataObject.deleteAll()
+            GlobalScope.launch {
+                database.dao().deleteAll()
+            }
+            setRecycler()
+        }
+
+        setRecycler()
+
+    }
+
+    fun setRecycler() {
+        recycler_view.adapter = Adapter(DataObject.getAllData())
+        recycler_view.layoutManager = LinearLayoutManager(this)
     }
 }
