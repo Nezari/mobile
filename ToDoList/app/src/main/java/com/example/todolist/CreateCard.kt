2edde23d19bfbe3d271451964eb2818098b35dbf
Nezari <Nezari@Nezari>
2023-12-25 00:37:38 +0700

    create class CreateCard

---
+++ ToDoList/app/src/main/java/com/example/todolist/CreateCard.kt
@@ -0,0 +1,36 @@
+package com.example.todolist
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_create_card.*
+
+class CreateCard : AppCompatActivity() {
+    private lateinit var database: Database
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_card)
+        database = Room.databaseBuilder(
+            applicationContext, Database::class.java, "to_do_db"
+        ).build()
+        save_button.setOnClickListener {
+            if (create_title.text.toString().trim { it <= ' ' }.isNotEmpty()
+                && create_priority.text.toString().trim { it <= ' ' }.isNotEmpty()
+            ) {
+                var title = create_title.getText().toString()
+                var priority = create_priority.getText().toString()
+                DataObject.setData(title, priority)
+                GlobalScope.launch {
+                    database.dao().insertTask(Entity(0, title, priority))
+
+                }
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
