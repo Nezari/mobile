f934806335832f79263f66f32b27196bfb82b789
Nezari <Nezari@Nezari>
2023-12-24 23:52:24 +0700

    rename to entity

--- ToDoList/app/src/main/java/com/example/todolist/Entity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/Entity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "to_do_table")
-data class ToDoListDbEntity(
+data class Entity(
     @PrimaryKey(autoGenerate = true)
     var id:Int,
     var title:String,
