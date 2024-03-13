0242f0e6e39de99c4c011c88aa0ef7cf665a147d
Nezari <Nezari@Nezari>
2024-03-13 19:16:21 +0700

    create local ds

---
+++ Watch/app/src/main/java/com/example/watch/models/LocalDataSource.kt
@@ -0,0 +1,34 @@
+package com.example.watch.models
+
+import android.app.Application
+import io.reactivex.Observable
+import kotlin.concurrent.thread
+
+class LocalDataSource(application: Application) {
+    private val movieDao: MovieDao
+    private val allMovies: Observable<List<Movie>>
+
+    init {
+        val db = LocalDatabase.getInstance(application)
+        movieDao = db.movieDao()
+        allMovies = movieDao.all
+    }
+
+    fun insert(movie: Movie) {
+        thread {
+            movieDao.insert(movie)
+        }
+    }
+
+    fun delete(movie: Movie) {
+        thread {
+            movieDao.delete(movie.id)
+        }
+    }
+
+    fun update(movie: Movie) {
+        thread {
+            movieDao.update(movie)
+        }
+    }
+}
