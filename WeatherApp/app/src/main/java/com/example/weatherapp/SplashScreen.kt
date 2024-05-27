500fc7f4833bbea14947c6ec1991e24403711a8c
Nezari <nezari@nezari>
2024-05-27 16:12:13 +0700

    create splash screen

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.weatherapp
+
+import android.animation.Animator
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val lottieAnimationView = findViewById<LottieAnimationView>(R.id.splash_screen)
+
+        lottieAnimationView.playAnimation()
+
+        lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {}
+
+            override fun onAnimationEnd(animation: Animator) {
+                val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(intent)
+                finish()            }
+
+            override fun onAnimationCancel(animation: Animator) {}
+
+            override fun onAnimationRepeat(animation: Animator) {}
+        })
+        lottieAnimationView.playAnimation()
+    }
+}
