5939460d83831f67f133545e2698313feb28192e
Nezari <nezari@nezari>
2024-05-27 16:10:56 +0700

    added view module

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.weatherapp.viewmodel
+
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.api.API_KEY
+import com.example.weatherapp.api.RetrofitClient
+import com.example.weatherapp.db.Weather
+import com.example.weatherapp.db.WeatherDatabase
+import com.example.weatherapp.db.WeatherRepository
+import com.example.weatherapp.forecast.Forecast
+import kotlinx.coroutines.launch
+import java.util.Timer
+import java.util.TimerTask
+
+class WeatherViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WeatherRepository
+    val weather: LiveData<List<Weather>>
+    val progressBarVisibility: MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        val weatherDao = WeatherDatabase.getInstance(application).weatherDao()
+        repository = WeatherRepository(weatherDao, RetrofitClient.create())
+        weather = repository.weather
+    }
+
+    fun refreshWeather(location: String, apiKey: String) {
+        viewModelScope.launch {
+            repository.refreshWeather(location, apiKey)
+            progressBarVisibility.value = View.GONE
+        }
+    }
+
+    fun updateWeather(apiKey: String) {
+        viewModelScope.launch {
+            val weatherList = repository.weather.value
+            if (weatherList != null) {
+                for (weather in weatherList) {
+                    repository.refreshWeather(weather.location, apiKey)
+                }
+            }
+        }
+
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                updateWeather(apiKey)
+            }
+        }, 5 * 60 * 1000)
+    }
+
+    fun deleteWeather(weather: Weather) {
+        viewModelScope.launch {
+            repository.deleteWeather(weather)
+        }
+    }
+
+    fun getForecast(location: String): LiveData<List<Forecast>> {
+        return liveData {
+            val forecast = repository.getForecast(location, API_KEY)
+            emit(forecast)
+        }
+    }
+}
