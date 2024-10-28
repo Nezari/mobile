bee2d2d05f91f65547dd5691f9f0b7e99154e694
Nezari <Nezari@Nezari>
2024-10-28 19:29:27 +0700

    Red: Test for null query fails

---
+++ TDD_RGR/app/src/test/java/com/example/tdd_rgr/UtilsTest.kt
@@ -0,0 +1,21 @@
+package com.example.tdd_rgr
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class UtilsTest {
+
+    @Test
+    fun testNullQuery() {
+        val nullResult = getSearchUrl(null)
+        if (nullResult == null) {
+            println("success")
+        } else {
+            println("error")
+        }
+    }
+
+    fun getSearchUrl(query: String?): String? {
+        return ""
+    }
+}
